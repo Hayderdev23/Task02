07549f73741c2d2f34b2bf52e82e153be9836334
Your Name <hayderwheed15@gmail.com>
2024-10-16 15:10:14 +0300

    first commit

---
+++ Controllers/Project.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AON_Freelancing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Project : ControllerBase
+    {
+        private static List<Project> project = new List<Project>();
+        [HttpGet]
+        public IActionResult getallproject()
+        {
+            return Ok(project);
+        }
+        [HttpPost]
+        public IActionResult StoreNewProject(Project project)
+        {
+            return Ok();
+
+        }
+        [HttpGet("{id}")]
+        public IActionResult GettheId(int id)
+        {
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteId(int id)
+        {
+            return Ok();
+        }
+
+    }
+}
+
+
---
+++ Model/Project.cs
@@ -0,0 +1,10 @@
+﻿namespace AON_Freelancing.Model
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        
+    }
+}
+
